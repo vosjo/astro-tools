7bb7696c5e9ddbbdd9faeaf2c4181c586a448092
Joris Vos <vos.joris@gmail.com>
2020-02-25 10:41:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Astro tools
+
+Collection of astronomy related command line scripts
---
+++ setup.py
@@ -0,0 +1,16 @@
+ 
+import setuptools
+
+setuptools.setup(
+    name="astro-tools", # Replace with your own username
+    version="0.0.1",
+    author="Joris Vos",
+    author_email="joris.vos@uv.cl",
+    description="Astronomy related scripts",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    entry_points = {
+        'console_scripts': ['astro-sfi=astro_tools.sfi:main'],
+    },
+    python_requires='>=2.7',
+)
